8fe7234e60604ddfa4d3a9b09f4fc6d91b59d29a
Manav Khandurie <manavkhandurie@gmail.com>
2024-07-04 04:18:06 +0530

    Added ComprehendService

--- server/ComprehendService/app.py
+++ server/ComprehendService/app.py
@@ -1,34 +1,45 @@
-from aws_lambda_powertools.event_handler import APIGatewayRestResolver
-from aws_lambda_powertools.utilities.typing import LambdaContext
-from aws_lambda_powertools.logging import correlation_paths
-from aws_lambda_powertools import Logger
-from aws_lambda_powertools import Tracer
-from aws_lambda_powertools import Metrics
-from aws_lambda_powertools.metrics import MetricUnit
+import json
+import boto3
+import os
 
-app = APIGatewayRestResolver()
-tracer = Tracer()
-logger = Logger()
-metrics = Metrics(namespace="Powertools")
-
-@app.get("/hello")
-@tracer.capture_method
-def hello():
-    # adding custom metrics
-    # See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/metrics/
-    metrics.add_metric(name="HelloWorldInvocations", unit=MetricUnit.Count, value=1)
-
-    # structured log
-    # See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/logger/
-    logger.info("Hello world API - HTTP 200")
-    return {"message": "hello world"}
-
-# Enrich logging with contextual information from Lambda
-@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
-# Adding tracer
-# See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/tracer/
-@tracer.capture_lambda_handler
-# ensures metrics are flushed upon request completion/failure and capturing ColdStart metric
-@metrics.log_metrics(capture_cold_start_metric=True)
-def lambda_handler(event: dict, context: LambdaContext) -> dict:
-    return app.resolve(event, context)
+def lambda_handler(event, context):
+    comprehend = boto3.client('comprehend')
+    
+    # Parse the incoming request
+    try:
+        body = event
+        text = body['text']
+        operation = body['operation']
+    except KeyError as e:
+        return {
+            'statusCode': 400,
+            'error': f'Missing parameter: {str(e)}'
+        }
+    
+    try:
+        if operation == 'sentiment':
+            response = comprehend.detect_sentiment(Text=text, LanguageCode='en')
+        elif operation == 'entities':
+            response = comprehend.detect_entities(Text=text, LanguageCode='en')
+        elif operation == 'key_phrases':
+            response = comprehend.detect_key_phrases(Text=text, LanguageCode='en')
+        elif operation == 'language':
+            response = comprehend.detect_dominant_language(Text=text)
+        elif operation == 'syntax':
+            response = comprehend.detect_syntax(Text=text, LanguageCode='en')
+        else:
+            return {
+                'statusCode': 400,
+                'error': 'Invalid operation requested'
+            }
+        
+        return {
+            'statusCode': 200,
+            'response' : response
+        }
+    
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'error': str(e)
+        }
